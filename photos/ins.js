db1c8e7a89c4e9d939890b282b74302bf9c9eb65
wonderxiao <942286384@qq.com>
2017-10-12 18:27:46 +0800

    Site updated: 2017-10-12 18:27:46

--- photos/ins.js
+++ photos/ins.js
@@ -116,9 +116,9 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/wonderxiao/wonderxiao.github.io/tree/master/min_photos/' + data.link[i];
+          var minSrc = 'https://github.com/wonderxiao/wonderxiao.github.io/tree/master/photos/min_photos/' + data.link[i];
   
-          var src = 'https://github.com/wonderxiao/wonderxiao.github.io/tree/master/min_photos/' + data.link[i];
+          var src = 'https://github.com/wonderxiao/wonderxiao.github.io/tree/master/photos/min_photos/' + data.link[i];
           
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
